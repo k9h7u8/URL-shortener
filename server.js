36650fe95e9e009fdd2aa50c0e4dac73f20f0266
Khushi Aggarwal <khushiaggarwal1302@gmail.com>
2023-02-09 19:18:39 +0530

    deployed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrls');
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
